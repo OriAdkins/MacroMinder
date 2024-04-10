a4e12fe5122d28da065af01890763235dd256e28
ryanrowe449 <43ryanrowe@gmail.com>
2024-04-09 21:23:53 -0400

    Implemented feature to allow users to add habits
    Added the /addhabit route, which renders AddHabit.html (GET) and adds a habit along with the associated user id of the user logged in to the Habits table in the database (POST), checking if the user is logged in and if the habit exists already beforehand
    Added a button ‘Add Habit’ to UserDashboard.html that calls the /addhabit route
    Altered UserDashboard.html to display habits of the logged in user; had to add a style to make the text black so it would be visible on the white box

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, flash, session, redirect, url_for
 from app import app, db, bcrypt
-from models import User
+from models import User, Habits
 
 # Render login page
 @app.route('/', methods=['GET'])
@@ -17,6 +17,8 @@ def login():
         existingUser = User.query.filter_by(username=username).first()
 
         if existingUser and bcrypt.check_password_hash(existingUser.password, password):
+            session['userid'] = existingUser.id  #store the user id in session
+            session['role'] = existingUser.role #store the user role in session
             # Check user role and redirect accordingly
             print("User role:", existingUser.role)  # Debug print to check the user's role
             if existingUser.role == 'Admin':
@@ -63,4 +65,37 @@ def lifecoach_dashboard():
 
 @app.route('/user/dashboard')
 def user_dashboard():
-    return render_template('UserDashboard.html')
+    userid = session.get('userid') #retrive userid from session; id of the logged in user
+    habits = Habits.query.filter_by(user_id=userid).all()
+    #load UserDashboard.html with habits
+    return render_template('UserDashboard.html', habits=habits)
+
+#route to render the addhabit page or add a habit
+@app.route('/addhabit', methods=['POST','GET'])
+def addHabit():
+    if request.method == 'POST':
+        description = request.form.get('habitdesc')
+        userid = session.get('userid')  #retrieve userid from session
+        role = session.get('role') #retrieve role from session
+        #if the user is logged in, check if the habit exists 
+        if userid:
+            existingHabit = Habits.query.filter_by(user_id=userid, habit_description=description).first()
+            #if it does not exist, add newHabit to the database. otherwise, notify user the habit exists
+            if existingHabit is None:
+                newHabit = Habits(user_id=userid, habit_description=description)
+                db.session.add(newHabit)
+                db.session.commit()
+                if role=='User':
+                    return redirect(url_for('user_dashboard'))  #back to the dashboard
+                elif role=='Admin':
+                    return redirect(url_for('admin_dashboard'))
+                else:
+                    return redirect(url_for('lifecoach_dashboard'))
+            else:
+                flash('This habit already exists')
+        else:
+            flash('You must be logged in to add a habit.')
+
+
+    # Render the AddHabit page (GET request or failed login)
+    return render_template('AddHabit.html')
