30390b460cc594b72a174c19fdd37f663154ea50
Ori Adkins <gda20b@fsu.edu>
2024-04-20 21:01:26 -0400

    add completionlog function
    added a function in the services for adding a completionlog

--- services/CompletionLogService.py
+++ services/CompletionLogService.py
@@ -8,3 +8,17 @@ class CompletionLogService:
         for log in logs:
             db.session.delete(log)
         db.session.commit()
+
+    @staticmethod
+    def add_completion_log(user_id, date, protein=0, calories=0, tasks_completed=0, weightlbs=150):
+        new_log = CompletionLog(
+            user_id=user_id,
+            date=date,
+            protein=protein,
+            calories=calories,
+            tasks_completed=tasks_completed,
+            weightlbs=weightlbs
+        )
+        db.session.add(new_log)
+        db.session.commit()
+        return new_log.tracking_id
