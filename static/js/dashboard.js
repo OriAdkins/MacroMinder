fc16b7bae4c2e494ab487f02438fe8eab075d7d1
Ori Adkins <gda20b@fsu.edu>
2024-04-24 19:48:15 -0400

    Added back JS function to restore edit/delete
    added back a JS function that was needed for edit and delete habit functionality

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -279,3 +279,11 @@ async function lifecoachLogMacros(event) {
 
     updateDate();
 }
+
+function getCurrentDateString() {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
